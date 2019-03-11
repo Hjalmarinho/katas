cb6a4864c805fab3e4308d2b453646f25e3bf6f0
unknown <oystein.molnes@gmail.com>
2019-03-11 13:45:14 +0100

    Find high and low from String of numbers

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Kata {
+
+    public static String highAndLow(String numbers) {
+        List<Integer> integers = Arrays.asList(numbers.split(" ")).stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return Collections.max(integers).toString() + " " + Collections.min(integers).toString() ;
+    }
 }
--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class KataTest {
+
+    @Test
+    public void highAndLow() {
+        System.out.println("HELLO");
+        assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
 }
