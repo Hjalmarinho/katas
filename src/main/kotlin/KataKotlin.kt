22a33026c7e1f63fb35e25edb45398aa6c1010c3
unknown <oystein.molnes@gmail.com>
2019-03-31 19:56:50 +0200

    Convert number to string

--- src/main/kotlin/KataKotlin.kt
+++ src/main/kotlin/KataKotlin.kt
@@ -2,3 +2,5 @@
     fun sumPositives(ints: IntArray): Int = ints.filter { it > 0 }.sum() // All types are objects
 
     fun reverseLetters(letters: String): String = letters.filter{it.isLetter()}.reversed()
+
+    fun numberToString(number: Int): String = number.toString()
--- src/test/kotlin/KataKotlinTest.kt
+++ src/test/kotlin/KataKotlinTest.kt
@@ -19,4 +19,11 @@ class KataKotlinTest {
         assertEquals("cba", reverseLetters("ab23c"))
         assertEquals("nahsirk", reverseLetters("krish21an"))
     }
+
+    @Test
+    fun testFixed() {
+        assertEquals("67", numberToString(67))
+        assertEquals("123", numberToString(123))
+        assertEquals("999", numberToString(999))
+    }
 }
