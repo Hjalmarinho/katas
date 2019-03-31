3584522a946d025311c1d6f2eccd8ddb7a53e1ff
unknown <oystein.molnes@gmail.com>
2019-03-31 19:53:39 +0200

    Kotlin refactor

--- src/main/kotlin/KataKotlin.kt
+++ src/main/kotlin/KataKotlin.kt
@@ -1,9 +1,4 @@
     // Since we are defining our method outside of a class, it represents a package-level function
-    fun sumPositives(ints: IntArray): Int {
-        // All types are objects
-        return ints.filter { it > 0 }.sum()
-    }
+    fun sumPositives(ints: IntArray): Int = ints.filter { it > 0 }.sum() // All types are objects
 
-    fun reverseLetters(letters: String): String {
-        return letters.filter{it.isLetter()}.reversed()
-    }
+    fun reverseLetters(letters: String): String = letters.filter{it.isLetter()}.reversed()
