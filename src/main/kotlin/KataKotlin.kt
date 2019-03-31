7fbe0769214ae13a79a02aa0ba8ef7f71e6ef3b1
unknown <oystein.molnes@gmail.com>
2019-03-31 18:29:13 +0200

    Kotlin sum positives

--- src/main/kotlin/KataKotlin.kt
+++ src/main/kotlin/KataKotlin.kt
@@ -1,3 +1,6 @@
-fun main(args : Array<String>) {
-    println("Hello, world!")
+
+// Since we are defining our method outside of a class, it represents a package-level function:
+// For now, Kotlin only supports references to top-level and local functions and members of classes, not individual instances.
+fun sumPositives(ints: IntArray): Int {
+    return ints.filter { int -> 0 < int  }.sum()
 }
--- src/test/kotlin/KataKotlinTest.kt
+++ src/test/kotlin/KataKotlinTest.kt
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class KataKotlinTest {
 
+    @Test
+    fun testSomething() {
+        assertEquals(15, sumPositives(intArrayOf(1, 2, 3, 4, 5)))
+        assertEquals(13, sumPositives(intArrayOf(1, -2, 3, 4, 5)))
+        assertEquals(0, sumPositives(intArrayOf()))
+        assertEquals(0, sumPositives(intArrayOf(-1, -2, -3, -4, -5)))
+        assertEquals(9, sumPositives(intArrayOf(-1, 2, 3, 4, -5)))
+    }
 }
